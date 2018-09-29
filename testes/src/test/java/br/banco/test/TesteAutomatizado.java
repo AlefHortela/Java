9d870184d59c9df22da07177c06837cecaded70d
Alef Hortelã <33780380+AlefHortela@users.noreply.github.com>
2018-09-29 16:04:35 -0300

    update antes de ir embora

--- testes/src/test/java/br/banco/test/TesteAutomatizado.java
+++ testes/src/test/java/br/banco/test/TesteAutomatizado.java
@@ -1,15 +1,20 @@
 package br.banco.test;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TesteAutomatizado {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver",
 				"/Users/fibbauru-15/Downloads/chromedriver");
-		
 		WebDriver driver = new ChromeDriver();	
-		
 		driver.get("http://www.google.com.br");
+		WebElement campoDeTexto = driver.findElement(By.name("q"));
+		campoDeTexto.sendKeys("javali");
+		campoDeTexto.submit();
+		driver.close();
+		
 	}
 }
